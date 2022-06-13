6351d8d324d8fe0966dcc0eb44d8e70114cf1d3b
Andrey Sakharov <sakharov.a@n-t.io>
2022-06-14 00:08:15 +0300

    just update a deoloy script by some test values

--- deploy/deploy_dna.ts
+++ deploy/deploy_dna.ts
@@ -10,13 +10,12 @@ const deployFunction: DeployFunction = async (hre) => {
 
 	const args = [
 		'0x760f17C973405E11559470B07a70e4f02db172E2', // owner
-		// 'QmTTS6eSoXdPceaXSYqgFgH2YU6Fd6RzAXMPJLnEK1QNpW', // folder my
-		'QmNNTm5UyZuknLvEvSUHMYeuSt5Q3CUv43CuUg6T8wKC26', // folder kuznetsov
-		100, // max count
-		1655078400, // presale start date 2022-06-14T20:00:00 UTC
-		1655139600, // sale start date 2022-06-22T20:00:00 UTC
-		parseEther('0.0005').toString(), // presale price
-		parseEther('0.0006').toString(), // sale price
+		'bafybeiajiadrxwjeh3vhcnziicqrovzcezdj7y6xjmg6o5lagourwggaem', // folder
+		5000, // max count
+		1655236800, // presale start date 2022-06-14T20:00:00 UTC
+		1655928000, // sale start date 2022-06-22T20:00:00 UTC
+		parseEther('0.5').toString(), // presale price
+		parseEther('0.6').toString(), // sale price
 	];
 
 	await deploy('DNA', {
